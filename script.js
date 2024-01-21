232e000532f4b2c047786ae4f3ad1c7319012520
OrlandoQ03 <oquin2003@gmail.com>
2024-01-20 23:56:27 -0800

    Created my first webpage for practice!

---
+++ script.js
@@ -0,0 +1,14 @@
+var number = 10;
+var string = 'Hello There';
+var isRad = true;
+
+if(number == 10) {
+    console.log("Yeah buddy!");
+}
+else {
+    console.log("Nope");
+}
+
+document.getElementById("Box").addEventListener("click", function() {
+    alert("I got clicked!");
+})
